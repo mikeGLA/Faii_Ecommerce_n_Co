dcda988448a7f87bb1f07ccc6a08a252aa7109a8
mikeGLA <mikecommand_conquer@hotmail.com>
2022-06-27 00:38:25 +0700

    update main page and card for product

--- src/components/MainPage.vue
+++ src/components/MainPage.vue
@@ -1,6 +1,5 @@
 <template>
   <div class="Main-Index">
-    <h1>Nong Faii "HOT" Glue</h1>
     <div class="Main-TopBar">
       <div class="TopBar-Greet">
         <p>Good Morning</p>
@@ -10,20 +9,32 @@
         <img :src="require('../assets/ShoppingCart.png')" :width="30" :height="30"/>
       </div>
     </div>
-    <input class="Main-SearchBox" placeholder="Search"/>
+    <div class="Main-SearchBox">
+      <img class="SearchBox-Icon" :src="require('../assets/magnifier.png')" :width="30" :height="30"/>
+      <input class="SearchBox-Index" placeholder="Search"/>
+    </div>
+    <div class="Catagory-Zone">
+      <h3>Adhesive Product</h3>
+    </div>
+    <div class="Card-Body">
+      <div><productCard1/></div>
+      <div><productCard1/></div>
+      <div><productCard1/></div>
+      <div><productCard1/></div>
+    </div>
   </div>
 </template>
 
 <script>
 // import ShoppingCart from "../assets/ShoppingCart.png"
 
+import productCard1 from "./ProductCard/productCard1.vue"
+
 export default {
   name: 'MainPage',
-  // data: function(){
-  //   return{
-  //     icon: ShoppingCart
-  //   }
-  // }
+  components: {
+    productCard1
+  }
 }
 </script>
 
@@ -31,7 +42,7 @@ export default {
   .Main-Index{
     text-align: center;
     height: 500px;
-    border: 1px solid red;
+    /* border: 1px solid red; */
   }
 
   .Main-TopBar{
@@ -57,11 +68,43 @@ export default {
   }
 
   .Main-SearchBox{
+    display:flex;
+    height: 50px;
+    width: 320px;
+    background: rgb(245, 245, 245);
+    justify-content: center;
+    border: 1px solid rgb(245, 245, 245);
+    margin-left: 15px;
+    border-radius: 10px;
+  }
+
+  .SearchBox-Icon{
+    padding-left: 30px;
+    margin-top: 10px;
+  }
+
+  .SearchBox-Index{
     width: 260px;
     height: 35px;
-    border: 1px solid grey;
+    border: 1px solid rgb(245, 245, 245);
+    background: rgb(245, 245, 245);
     border-radius: 10px;
     margin: auto;
-    
+    margin-right: 30px;
+  }
+
+  .Card-Body{
+    background: rgb(242, 242, 242);
+    margin-left: 10px;
+    margin-right: 10px;
+    /* border: 1px solid red; */
+    display: inline-flex;
+    flex-wrap: wrap;
+    flex-direction: row;
+    justify-content: space-evenly;
+  }
+
+  .Card-Body > div{
+    margin: 5px;
   }
 </style>
---
+++ src/components/ProductCard/productCard1.vue
@@ -0,0 +1,63 @@
+<template>
+    <div class="Card-Index">
+        <div class="Product-Picture">
+            <img :src="require('../../assets/TestCard.jpg')" :width="120" :height="120">
+        </div>
+        <div class="Product-Name">Shiba Doggi</div>
+        <div class="Product-Price-Sold">696969 Baht</div>
+        <div class="Product-Incart-Count">{{counting}} In Cart</div>
+        <button @click="addCart()" class="Product-Add">Add</button>
+        <button @click="RemoveCart()" class="Product-Rem">Remove</button>
+    </div>
+</template>
+
+<script>
+
+export default {
+    name: 'productCard1',
+    data(){
+      return{
+        counting: 0,
+      }
+    },
+    methods:{
+        addCart(){
+            this.counting++
+        },
+        RemoveCart(){
+            this.counting--
+        }
+    },
+}
+
+</script>
+
+<style>
+    .Card-Index{
+        text-align: center;
+        width: 140px;
+        height: 220px;
+        background: white;
+        display: flex;
+        flex-direction: column;
+        justify-content: space-evenly;
+    }
+
+    .Product-Incart-Count{
+        color: blue;
+    }
+
+    .Product-Add{
+        border: 1px solid rgb(160, 213, 255);
+        background: rgb(137, 203, 255);
+        width: 100px;
+        margin: auto;
+    }
+
+    .Product-Rem{
+        border: 1px solid rgb(160, 213, 255);
+        background: red;
+        width: 100px;
+        margin: auto;
+    }
+</style>
